0ee72dd2828d2ee8b3ec3b5b9086d39b4534c9f6
Jayden Bailey <jaydenkieran@gmail.com>
2019-02-06 13:17:57 +0000

    working implementation

--- SpecialLookup_body.php
+++ SpecialLookup_body.php
@@ -1,12 +1,8 @@
 <?php
 
-use SMW\ApplicationFactory;
-use SMWQueryProcessor;
-use SMWQuery;
-
 class SpecialLookup extends SpecialPage {
 	function __construct() {
-		parent::__construct( 'Lookup' );
+		parent::__construct( 'Lookup', '', false );
 	}
 
 	function execute( $par ) {
@@ -18,40 +14,82 @@ class SpecialLookup extends SpecialPage {
     $id = $request->getText( 'id', '' );
     $name = $request->getText( 'name', '' );
 
+    if ( empty( $id ) ) {
+      // No ID given
+      $this->backupPlan( $name );
+    }
+
+    $prop = '';
+
+    switch ( $type ) {
+      case 'item':
+        $prop = 'Item ID';
+        break;
+      case 'npc':
+        $prop = 'NPC ID';
+        break;
+      case 'object':
+        $prop = 'Object ID';
+        break;
+    }
+
+    if ( empty( $prop ) ) {
+      // Not a recognised type
+      $this->backupPlan( $name );
+    } else {
+      $result = $this->makeSMWQuery( $prop, $id );
+      if ( !$result['query']['results'] ) {
+        // No results from SMW, check if name provided is a wiki page
+        $this->backupPlan( $name );
+      } else {
+        $this->handleSMWResult( $result );
+      }
+    }
+  }
+
+  protected function handleSMWResult( $result ) {
+    $r = reset($result['query']['results']);
+
+    // TODO: handle version anchor
+    // $r['printouts']['Version anchor']
+
+    // Redirect to the target page
+    $this->getOutput()->redirect($r['fullurl']);
+  }
+
+  protected function backupPlan( $name ) {
+    $output = $this->getOutput();
     if ( empty( $name ) ) {
       // Redirect to the main page if there's no name
       $mp = Title::newMainPage();
       $url = $mp->getFullURL();
       $output->redirect( $url );
-    }
-
-    switch ( $type ) {
-      case 'item':
-        $query = $this->getSMWQuery( '[[Item ID::' . $id . ']]|?Item ID|?Version anchor' );
-        $result = $this->getSMWQueryResult( $query );
-        $output->addWikitext( $result );
-      case 'npc':
-      case 'object':
-      default:
-        // Try seeing if name exists as a page
-        // if not, redir to Special:Search
+    } else {
+      $title = Title::newFromText( $name );
+      if ( $title->exists() ) {
+        // Page exists, let's redirect to it
+        $url = $title->getFullURL();
+        $output->redirect( $url );
+      } else {
+        // Page doesn't exist, let's redirect to the search page
+        $search = Title::newFromText( 'Special:Search' );
+        $url = $search->getFullURL( 'search=' . $name );
+        $output->redirect( $url );
+      }
     }
   }
   
-  protected function getSMWQuery( $queryString, array $printouts, array $parameters = [] ) {
-		SMWQueryProcessor::addThisPrintout( $printouts, $parameters );
-		$query = SMWQueryProcessor::createQuery(
-			$queryString,
-			SMWQueryProcessor::getProcessedParams( $parameters, $printouts ),
-			SMWQueryProcessor::SPECIAL_PAGE,
-			'',
-			$printouts
-		);
-		$query->setOption( SMWQuery::PROC_CONTEXT, 'API' );
-		return $query;
-  }
-
-  protected function getSMWQueryResult ( SMWQuery $query ) {
-    return ApplicationFactory::getInstance()->getStore()->getQueryResult( $query );
+  protected function makeSMWQuery( $prop, $id ) {
+    $params = new DerivativeRequest(
+      $this->getRequest(),
+      array(
+        'action' => 'ask',
+        'query' => '[[' . $prop . '::' . $id . ']]|?Version anchor'
+      )
+    );
+    $api = new ApiMain( $params );
+    $api->execute();
+    $result = $api->getResult()->getResultData();
+    return $result;
   }
 }
